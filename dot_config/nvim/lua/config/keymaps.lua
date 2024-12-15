bbe7d4cd1e8b5372f56d8db8b5028893d6139624
Radek Tomšej <radek@tomsej.cz>
2024-12-15 22:48:23 +0100

    refactor: Update Neovim keymaps for delete and change operations in visual mode

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -6,17 +6,12 @@ vim.api.nvim_set_keymap("n", "d", '"dd', { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "dd", '"ddd', { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "D", '"dD', { noremap = true, silent = true })
 
+vim.api.nvim_set_keymap("v", "d", '"dd', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("v", "dd", '"ddd', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("v", "D", '"dD', { noremap = true, silent = true })
+
 vim.api.nvim_set_keymap("n", "c", '"cc', { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "C", '"cC', { noremap = true, silent = true })
-
--- DBT
-vim.keymap.set("n", "<leader>tc", ":DbtCompile<CR>", { noremap = true, silent = true })
-vim.keymap.set("n", "<leader>ty", ":DbtModelYaml<CR>", { noremap = true, silent = true })
-vim.keymap.set("n", "<leader>tr", ":DbtRun<CR>", { noremap = true, silent = true })
-vim.keymap.set("n", "<leader>tf", ":DbtRunFull<CR>", { noremap = true, silent = true })
-vim.keymap.set("n", "<leader>tt", ":DbtTest<CR>", { noremap = true, silent = true })
-vim.keymap.set("n", "<leader>t+", ":DbtListUpstreamModels<CR>", { noremap = true, silent = true })
-vim.keymap.set("n", "<leader>t-", ":DbtListDownstreamModels<CR>", { noremap = true, silent = true })
 
 --: home row goto end and start of line (same as in Helix editor) {{{
 vim.keymap.set({ "n", "v", "o" }, "gh", "^", { desc = "Go to beginning of line" })
